22d93e0bbf1c7ee381db5a7984cb5de51c006fd7
Pablo Jara <pablojaramunoz@icloud.com>
2024-02-15 01:46:01 -0300

    post

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 // Endpoint para obtener datos de la API
 app.get('/data', async (req, res) => {
     try {
@@ -14,6 +15,18 @@ app.get('/data', async (req, res) => {
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
+
+app.post('/data', async (req, res) => {
+    try {
+        // Aquí puedes manejar la lógica para procesar los datos recibidos en la solicitud POST
+        // Por ejemplo, podrías guardarlos en una base de datos, etc.
+        console.log('Data received:', req.body);
+        res.status(200).json({ message: 'Data received successfully' });
+    } catch (error) {
+        console.error('Error processing data:', error);
+        res.status(500).json({ error: 'Failed to process data' });
     }
 });
 
